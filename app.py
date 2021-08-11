0f1ba97e7b749fa9fd2dc956c84ceec0916ee24d
m990 <maxn990@icloud.com>
2021-08-11 07:07:35 -1000

    update requirements and reinstall dependencies

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ from clarifai_grpc.grpc.api import service_pb2_grpc
 stub = service_pb2_grpc.V2Stub(ClarifaiChannel.get_grpc_channel())
 metadata = (('authorization', 'Key 24f6926e2f1744aa8e83199cb554d8b0'),)
 
-# from clarifai.rest import Image as ClImage
-
 app = Flask(__name__)
 
 upload_folder = "static/uploads"
@@ -18,9 +16,6 @@ allowed_extentions = set(['png', 'jpeg', 'jpg'])
 
 app.config['UPLOAD_FOLDER'] = upload_folder
 app.config['SECRET_KEY'] = 'fjhasldkfjhasdflkhasdflkjh'
-
-# mlApp = ClarifaiApp(api_key='24f6926e2f1744aa8e83199cb554d8b0')
-# model = mlApp.public_models.general_model
 
 recyclable_objects = ["bottle", "cardboard", "glass", "plastic bottle", "plastic container", "cereal box",
                      "snack box", "phonebook", "magazine", "mail", "paper", "newspaper", "tin cans",
@@ -70,72 +65,60 @@ def is_not_recyclable(detected_objects):
 def index():
     completed = False
     if request.method == 'POST':
-        # try:
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('Please select a file.')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            position = upload_folder + "/" + filename
-            # img = ClImage(filename=position)
-            # response = model.predict([img])
-            # results = response["outputs"][0]["data"]["concepts"]
-            with open(position, "rb") as file:
-                file_bytes = file.read()
-            api_request = service_pb2.PostModelOutputsRequest(
-                model_id='aaa03c23b3724a16a56b629203edc62c',
-                inputs=[
-                # image stuff may need to be different so that we can use the user's uploaded images instead of a URL
-                  resources_pb2.Input(data=resources_pb2.Data(image=resources_pb2.Image(base64=file_bytes)))
-                ])
-            response = stub.PostModelOutputs(api_request, metadata=metadata)
-            if response.status.code != status_code_pb2.SUCCESS:
-                flash("API request failed")
-            correct_results = []
-            # for i in results:
-            #     name = i["name"]
-            #     value = round((i["value"]) * 100)
-            #     if (value > 85):
-            #         correct_results.append(name)
-            print(response)
-            for concept in response.outputs[0].data.concepts:
-                # print('%12s: %.2f' % (concept.name, concept.value))
-                value = round(concept.value * 100)
-                if value > 85:
-                    correct_results.append(concept.name)
-            # str(response["outputs"][0]["data"]["concepts"][0]["value"]*100)
-            percent_sure = "We are " + str((response.outputs[0].data.concepts[0].value)*100) + "% sure that your object is recyclable!"
-            os.remove(position)
-            if is_recyclable(correct_results):
-                recyclable = "Please recycle your object."
-                flash("Success! Press \"See results\" to see our analysis of your item.")
-                return render_template("index.html", recyclable=recyclable, percent_sure=percent_sure,
-                 completed=True, isRecyclable=True)
-            elif is_not_recyclable(correct_results):
-                recyclable = "Contrary to popular belief, your object is not recyclable."
-                flash("Success! Press \"See results\" to see our analysis of your item.")
-                return render_template("index.html", recyclable=recyclable, percent_sure=percent_sure,
-                 completed=True, isRecyclable=False)
-            elif tech_is_recyclable(correct_results):
-                recyclable = "Your object can be recycled at any technology recycling place, or your nearest Apple Store for free."
-                flash("Success! Press \"See results\" to see our analysis of your item.")
-                return render_template("index.html", recyclable=recyclable, percent_sure=percent_sure,
-                 completed=True, isRecyclable=False)
+        try:
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            if file.filename == '':
+                flash('Please select a file.')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                position = upload_folder + "/" + filename
+                with open(position, "rb") as file:
+                    file_bytes = file.read()
+                api_request = service_pb2.PostModelOutputsRequest(
+                    model_id='aaa03c23b3724a16a56b629203edc62c',
+                    inputs=[
+                      resources_pb2.Input(data=resources_pb2.Data(image=resources_pb2.Image(base64=file_bytes)))
+                    ])
+                response = stub.PostModelOutputs(api_request, metadata=metadata)
+                if response.status.code != status_code_pb2.SUCCESS:
+                    flash("API request failed")
+                correct_results = []
+                for concept in response.outputs[0].data.concepts:
+                    value = round(concept.value * 100)
+                    if value > 85:
+                        correct_results.append(concept.name)
+                percent_sure = "We are " + str((response.outputs[0].data.concepts[0].value)*100) + "% sure that your object is recyclable!"
+                os.remove(position)
+                if is_recyclable(correct_results):
+                    recyclable = "Please recycle your object."
+                    flash("Success! Press \"See results\" to see our analysis of your item.")
+                    return render_template("index.html", recyclable=recyclable, percent_sure=percent_sure,
+                     completed=True, isRecyclable=True)
+                elif is_not_recyclable(correct_results):
+                    recyclable = "Contrary to popular belief, your object is not recyclable."
+                    flash("Success! Press \"See results\" to see our analysis of your item.")
+                    return render_template("index.html", recyclable=recyclable, percent_sure=percent_sure,
+                     completed=True, isRecyclable=False)
+                elif tech_is_recyclable(correct_results):
+                    recyclable = "Your object can be recycled at any technology recycling place, or your nearest Apple Store for free."
+                    flash("Success! Press \"See results\" to see our analysis of your item.")
+                    return render_template("index.html", recyclable=recyclable, percent_sure=percent_sure,
+                     completed=True, isRecyclable=False)
+                else:
+                    flash("Success! Press \"See results\" to see our analysis of your item.")
+                    recyclable = "Your object is not recyclable."
+                    percent_sure = ""
+                    return render_template("index.html", percent_sure=percent_sure,
+                     recyclable=recyclable, completed=True, isRecyclable=False)
             else:
-                flash("Success! Press \"See results\" to see our analysis of your item.")
-                recyclable = "Your object is not recyclable."
-                percent_sure = ""
-                return render_template("index.html", percent_sure=percent_sure,
-                 recyclable=recyclable, completed=True, isRecyclable=False)
-        else:
-            flash("File type not supported. Please upload a png, jpg or a jpeg.")
-        # except:
-        #     flash("Please try a different file.")
+                flash("File type not supported. Please upload a png, jpg or a jpeg.")
+        except:
+            flash("Please try a different file.")
     return render_template("index.html", recyclable="", percent_sure="", completed=False, isRecyclable=False)
 
 @app.route('/our-team')
